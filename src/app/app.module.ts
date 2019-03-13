cf90cf0d85a3184a05fda1afc1e02b3d8caa289a
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-13 09:57:46 +0100

    Creamos las rutas y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
+import { RouterModule } from '@angular/router';
+//importar rutas
+import { ROUTES } from './app.routes';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +20,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash: true} )
   ],
   providers: [],
   bootstrap: [AppComponent]
