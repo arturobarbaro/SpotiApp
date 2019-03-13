6f0708c2fa2dadba1e213c9a452656f45c0e4fb6
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-13 13:02:31 +0100

    Optimizacion de las peticiones http

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,22 +11,23 @@ export class SpotifyService {
 
   }
 
-  getNewReleases(){
+  getQuery( query: string){
+      const url=`https://api.spotify.com/v1/${query}`;
 
       const headers = new HttpHeaders({
           'Authorization': 'Bearer BQB0ARn2Oyai-i7YPBeREcqVP4COf_3zrEtZ82M8rus45ZY1BFlDq5WDFvbY3PJU1WLpe5cOqUV0Ao1mBqzMf8vPJmkcsCcBj54heeENHXirCG4ErzEnzTc-NMGLlwI8d8ziNTUqrGw'
-      })
+      });
 
-      return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-           .pipe( map( data =>  data['albums'].items));
+      return this.http.get(url,{headers});
+  }
+
+  getNewReleases(){
+      return this.getQuery('browse/new-releases')
+                 .pipe( map( data =>  data['albums'].items));
   }
 
   getArtista( termino:string){
-      const headers = new HttpHeaders({
-          'Authorization': 'Bearer BQCrXCedvCE9TO0i Iv4QbYY_-k3_JZi-it6SOW7ejMK7qD3h4SGpf1RWgDOokb60TTOfDrvz19EEf5B5rK8xDG-KPQxiHF44AH9zKgJpmC9fUBG_XuRFKeF0sS_ckqTvfL31Vrrnbbs'
-      })
-
-      return this.http.get(`https://api.spotify.com/v1/search?query=${termino}&type=artist&offset=0&limit=15`, { headers })
-            .pipe( map( data =>  data['artists'].items));
+      return this.getQuery(`search?query=${termino}&type=artist&offset=0&limit=15`)
+                 .pipe( map( data =>  data['artists'].items));
   }
 }
