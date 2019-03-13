bef720fd3f1684c8c592c3efc6579fec93d3b1e7
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-13 10:51:08 +0100

    Importamos informacion y moficamos estilos

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -16,9 +16,9 @@ export class SpotifyService {
           'Authorization': 'Bearer BQCrXCedvCE9TO0iIv4QbYY_-k3_JZi-it6SOW7ejMK7qD3h4SGpf1RWgDOokb60TTOfDrvz19EEf5B5rK8xDG-KPQxiHF44AH9zKgJpmC9fUBG_XuRFKeF0sS_ckqTvfL31Vrrnbbs'
       })
 
-      this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-           .subscribe( data =>{
-               console.log(data)
-           })
+      return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+           // .subscribe( data =>{
+           //     console.log(data)
+           // })
   }
 }
