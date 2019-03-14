001108f98eb5f6cf3048d203fb771808a791a965
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-14 08:35:29 +0100

    Rutas pasando el id del artista

--- src/app/components/tarjeta/tarjeta.component.ts
+++ src/app/components/tarjeta/tarjeta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tarjeta',
@@ -8,9 +10,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TarjetaComponent implements OnInit {
 
   @Input() items:any[]=[];
-  constructor() { }
+  constructor( private router:Router ) { }
 
   ngOnInit() {
+  }
+
+  verArtista(item:any){
+      (item.type=='artist')?this.router.navigate(['/artist',item.id]):this.router.navigate(['/artist',item.artists[0].id])
   }
 
 }
